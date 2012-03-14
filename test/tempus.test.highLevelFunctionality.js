52a134c434e8d1725ad1791dadd145ab8d9b65c9
Keith Cirkel <github@keithcirkel.co.uk>
2012-03-14 23:05:21 +0000

    Added additional calendar test to high level tests

--- test/tempus.test.highLevelFunctionality.js
+++ test/tempus.test.highLevelFunctionality.js
@@ -43,4 +43,25 @@ QUnit.test('making a calendar', function () {
             [ 26, 27, 28, 29, 30, 31,  1 ]
         ],
         'Calendar representation of month');
+
+    days = [];
+    newdate = new Tempus(2011, 11, 1);
+    newdate.eachWeekOfMonth(function (week, week_newdate) {
+        var subDays = [];
+        week_newdate.eachISODayOfWeek(function (day, day_newdate) {
+          subDays.push(day_newdate.date());
+        });
+        days.push(subDays);
+    });
+
+    deepEqual(days,
+        [
+            // M   T   W   T   F   S   S //
+            [ 28, 29, 30,  1,  2,  3,  4 ],
+            [  5,  6,  7,  8,  9, 10, 11 ],
+            [ 12, 13, 14, 15, 16, 17, 18 ],
+            [ 19, 20, 21, 22, 23, 24, 25 ],
+            [ 26, 27, 28, 29, 30, 31,  1 ]
+        ],
+        'Calendar representation of month');
 });
