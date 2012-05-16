d862d759af3542806183fc36dcc59ec5f31e7531
Keith Cirkel <github@keithcirkel.co.uk>
2012-05-16 08:33:49 +0100

    Add Tempus.Interval
    Tempus.Interval takes two dates and will give you information on the
    differences. Most notibly it works with ISO intervals - allowing you to input
    and output ISO8601 interval strings. It also has a human readable toString()
    method.

---
+++ tempus.interval.js
@@ -0,0 +1,227 @@
+/*jslint laxcomma: true */
+/**
+ * Tempus - Time for a new Date()
+ *
+ * Tempus is a full replacement for Date(), the standard Date class in JavaScript.
+ *
+ * Tempus.interval module is for easily manipulating the difference between 2 dates
+ *
+ * This code is licensed under the MIT
+ * For the full license see: http://keithamus.mit-license.org/
+ * For more information see: http://keithamus.github.com/tempus
+ *
+ * @author Keith Cirkel ('keithamus') <tempus@keithcirkel.co.uk>
+ * @license http://keithamus.mit-license.org/
+ * @copyright (C) 2012, Keith Cirkel
+ *
+ */
+ (function (global, Tempus, ArSlice, TYPE_STRING, TYPE_FUNCTION, TYPE_NUMBER, TYPE_ARRAY, TYPE_REGEXP, realTypeOf, undef) {
+    // ^ Get some methods from the global object, close scope on them for protection
+
+    var TIProto
+    ,   ISOIntervalFragmentRegex = /^(\d+)([,\.]\d+)?(\w)/
+    ,   ISOIntervalRegExp = /^([\+\-])?P((?:\d+(?:[,\.]\d+)?[YMWD])+)?(?:T((?:\d+(?:[,\.]\d+)?[HMS])+))?$/
+    ,   intvlShorthand = ['Y', 'M', 'D', 'TH', 'TM', 'TS', 'TMS']
+    ,   intvlSuffxies = ['%nY', '%nM', '%nD', '%nH', '%nM', '%nS', '%nMS']
+    ,   intvlMethods = {
+            'Y': 'FullYear',
+            'M': 'Month',
+            'D': 'Date',
+            'TH': 'Hours',
+            'TM': 'Minutes',
+            'TS': 'Seconds',
+            'TMS': 'Milliseconds'
+        }
+    ,   intvlUpperVals = [12, 30, 24, 60, 60, 1000, 1000]
+    ,   intvlCount = intvlShorthand.length
+    ,   intvlLOCALES = {};
+
+    function intvltoString(dateQualifiers, timeSep, fragSep) {
+        var fragments = []
+            ,   value
+            ,   neg = this.valueOf() < 0
+            ,   overlap = 0
+            ,   i = intvlCount;
+            fragSep = fragSep || '';
+
+            if (neg) this.swapDates();
+
+            while(i--) {
+                value = this['diff' + intvlMethods[intvlShorthand[i]]]() - overlap;
+                overlap = 0;
+                if (value) {
+                    // Value is negative
+                    if (value < 0) {
+                        value = intvlUpperVals[i-1] + value - overlap;
+                        overlap = 1;
+                    }
+                    fragments.unshift(((dateQualifiers[value] || dateQualifiers[0])[i]).replace('%n', value));
+                }
+                if (i == 3 && timeSep) fragments.unshift(timeSep);
+            }
+
+            if (neg) this.swapDates();
+
+            if (fragments[fragments.length-1] == timeSep) fragments.pop();
+
+            return fragments.join(fragSep);
+    }
+
+    
+    function TempusInterval() {
+        // If this was called without "new " then be friendly and do it for the user.
+        if (!(this instanceof TempusInterval)) return new TempusInterval(arguments);
+
+        // If we were only given 1 argument - an array - then use
+        // that as the arguments otherwise just use arguments.
+        var ar = !(1 in arguments) && /ar/.test(realTypeOf(arguments[0])) ? arguments[0] : arguments;
+
+        // Set the "date from"
+        this._dF = new Tempus(ar[0]);
+
+        // Set the "date to"
+        this._dT = this._dF.clone().set(ar[1]);
+
+        // Set the LOCALE from the base LOCALE. This way we can have per-instance LOCALEs
+        this._l = Tempus.LOCALE;
+
+        return this;
+    }
+
+    /**
+     * Add a locale to Tempus.Interval
+     *
+     * @param {String} localeName The name of your locale, e.g 'en-GB', 'en-US'
+     * @param {Array} qualifiers 2d array, Names for each quantifying unit, from '%i year' down to '%i millisecond'
+     * @param {Array} timing Suffix/Prefix for timing: 0: '%d ago', 1: 'in %d', 2: 'about %s'
+     * @param {Array} unitSeparator Punctuation for separation: ', '
+     *
+     */
+    TempusInterval.addLocale = function (localeName, qualifiers, timing, unitSeparator) {
+        intvlLOCALES[localeName] = {
+            Q: qualifiers,
+            T: timing,
+            S: unitSeparator
+        };
+    };
+    
+    TIProto = TempusInterval.prototype = {
+        
+        valueOf: function () {
+            return +(this._dT) - +(this._dF);
+        },
+        
+        toString: function (format) {
+            return intvlLOCALES[this._l].T[+(this.valueOf() > 0)].replace('%d', intvltoString.call(this,
+                intvlLOCALES[this._l].Q, // Qualifiers
+                '',                      // No time separator
+                intvlLOCALES[this._l].S  // Unit separator
+            ));
+        },
+
+        swapDates: function () {
+            var value = this._dT;
+            this._dT = this._dF;
+            this._dF = value;
+            return this;
+        },
+        
+        toISOString: function () {
+            return (this.valueOf() < 0 ? '-' : '') + 'P' + intvltoString.call(this,
+                [intvlSuffxies], // Qualifiers
+                'T'              // 'T' time separator
+            );
+        }
+
+        
+    };
+
+    TIProto.toJSON = TIProto.toISOString;
+
+    function PSetIntvMethod(meth) {
+        TIProto['diff' + meth] = function () {
+            return this._dT['UTC' + meth]() - this._dF['UTC' + meth]();
+        };
+    }
+
+    for(var meth in intvlMethods) PSetIntvMethod(intvlMethods[meth]);
+    
+    /***********************************************/
+    /*           ISO8601 Interval Parser           */
+    /*               (e.g +P3Y4M2D))               */
+    /***********************************************/
+    
+    function parseISOInterveralFragment(matches, neg, isTime) {
+        var i = matches.length
+        ,   n;
+        
+        neg = neg ? 'sub' : 'add';
+        
+        // Loop through the matches...
+        while(i--) {
+            // value is the match (e.g. 3D) minus the last letter (e.g 3)
+            matches[i] = matches[i].match(ISOIntervalFragmentRegex);
+            
+            // If we're looking at Times, add a T to the beginning of prop
+            if (isTime) matches[i][3] = 'T' + matches[i][3];
+            
+            // Treat Weeks specially
+            if (matches[i][3] === 'W') {
+                this[neg + intvlMethods.D](+maches[i][0] * 7);
+            } else {
+                
+                // Set the property using "subValue() or addValue()"
+                this[ neg + intvlMethods[matches[i][3]] ].call(this, +matches[i][1]);
+                
+                // If we have a decimal, look for the next property in the line
+                if ((+matches[i][2] || 0) > 0) {
+                    // Parse the split decimal also:
+                    n = AindexOf(intvlShorthand, +matches[i][3]) + 1 || intvlShorthand.length;
+                    
+                    this[ neg + intvlMethods[ intvlShorthand[n] ] ](+matches[i][1] * intvlUpperVals[n]);
+                }
+            }
+        }
+    }
+    
+    Tempus.addParser(
+         // Our test function, returns true if the string a matches our regexp
+        function (interval_string) {
+            return ISOIntervalRegExp.test(interval_string);
+        },
+        // Our parser function
+        function (intveral_string) {
+            var matches;
+
+            // Apply additional arguments from 1st, allowing functions like:
+            // Tempus('P3D', 2012, 01, 01); -> returns 4th Jan
+            // If there arent any more args, then we get todays date, which we need
+            if (1 in arguments) this.set.apply(this, ArSlice.call(arguments, 1));
+            
+            // Our regex to split up the neg/pos, day, and time values.
+            if (!(intveral_string = (''+intveral_string).match(ISOIntervalRegExp))) return false;
+            
+            // Match the date portion:
+            if (intveral_string[2] && (matches = intveral_string[2].match(/\d+(?:[,\.]\d+)?[YMWD]/g))) {
+                parseISOInterveralFragment.call(this, matches, intveral_string[1] == '-');
+            }
+            // Match the time portion:
+            if (intveral_string[3] && (matches = intveral_string[3].match(/\d+(?:[,\.]\d+)?[HMS]/g))) {
+                parseISOInterveralFragment.call(this, matches, intveral_string[1] == '-', true);
+            }
+            return this;
+        },
+        // We expect 1st arg to be string
+        TYPE_STRING
+    );
+
+    // Add locale data:
+    var pqualifiers = [], qualifiers = [
+        '%n year', '%n month', '%n day',
+        '%n hour', '%n minute', '%n second', '%n millisecond'
+    ];
+    for(i = 0; i < qualifiers.length; ++i) pqualifiers[i] = qualifiers[i] + 's';
+    TempusInterval.addLocale('en', [pqualifiers, qualifiers], ['%d ago', 'in %d', 'about %s'], ', ');
+    Tempus.Interval = TempusInterval;
+
+}(this, Tempus, ([]).slice, 'string', 'function', 'number', 'array', 'regexp', Tempus.util.realTypeOf));
---
+++ test/tempus.test.interval.js
@@ -0,0 +1,268 @@
+QUnit.module('Interval');
+// Clean up coverage for Tempus.*
+covers(Tempus, 'Tempus.*', 'Interval');
+
+covers(Tempus.Interval.prototype, 'Interval', 'constructor');
+QUnit.test("new Interval()", function () {
+    
+    var interval = new Tempus.Interval(new Tempus(), new Tempus());
+
+    ok(interval instanceof Tempus.Interval, 'Interval returns interval');
+
+    raises(function () {
+        new Tempus.Interval();
+    },
+    /^Error: Invalid Date$/, 'Tempus.Interval() raises "Invalid Date" with no arguments');
+
+});
+
+QUnit.test("Interval() as a factory method", function () {
+    
+    var interval = Tempus.Interval(new Tempus(), new Tempus());
+
+    ok(interval instanceof Tempus.Interval, 'Interval returns interval');
+
+    raises(function () {
+        Tempus.Interval();
+    },
+    /^Error: Invalid Date$/, 'Tempus.Interval() raises "Invalid Date" with no arguments');
+
+});
+
+covers(Tempus.Interval.prototype, 'Interval', 'valueOf');
+QUnit.test('valueOf()', function () {
+
+    var interval = Tempus.Interval(new Tempus(), new Tempus(+(new Tempus())+1));
+    equal(interval.valueOf(), 1, 'valueOf() gets time difference in MS');
+
+    interval = Tempus.Interval(new Tempus(), new Tempus(+(new Tempus())+600));
+    equal(interval.valueOf(), 600, 'valueOf() gets time difference in MS');
+
+    interval = Tempus.Interval(new Tempus(0), new Tempus(100));
+    equal(interval.valueOf(), 100, 'valueOf() gets time difference in MS');
+
+    interval = Tempus.Interval(new Tempus(942520000), new Tempus(942523519));
+    equal(interval.valueOf(), 3519, 'valueOf() gets time difference in MS');
+
+    interval = Tempus.Interval(new Tempus(942523519), new Tempus(942520000));
+    equal(interval.valueOf(), -3519, 'valueOf() always does from - to');
+});
+
+QUnit.test("Interval() takes Tempus dates as args", function () {
+
+    equal(+(new Tempus.Interval(942520000, 942523519)), 3519, 'Interval can set on numbers like Tempus');
+    equal(+(new Tempus.Interval('Sun, 11 Sep 2011 21:48:43 GMT', 'Sun, 11 Sep 2011 21:48:44 GMT')), 1000, 'Interval can set on strings like Tempus');
+    equal(+(new Tempus.Interval('2012-12-21T06:06:06.123+0000', '2012-12-21T06:06:06.124+0000')), 1, 'Interval can set on strings like Tempus');
+
+    equal(+(new Tempus.Interval([2011, 8], new Tempus())), 942523519, 'Interval can set on arrays like Tempus');
+    equal(+(new Tempus.Interval([2011, 8, 11], new Tempus())), 78523519, 'Interval can set on arrays like Tempus');
+    equal(+(new Tempus.Interval([2011, 8, 11, 21], new Tempus())), 2923519, 'Interval can set on arrays like Tempus');
+    equal(+(new Tempus.Interval([2011, 8, 11, 21, 48], new Tempus())), 43519, 'Interval can set on arrays like Tempus');
+    equal(+(new Tempus.Interval([2011, 8, 11, 21, 48, 43], new Tempus())), 519, 'Interval can set on arrays like Tempus');
+    equal(+(new Tempus.Interval([2011, 8, 11, 21, 48, 43, 519], new Tempus())), 0, 'Interval can set on arrays like Tempus');
+
+    Tempus.addParser(function () { return true; }, function (ob) {
+        return this.set.apply(this, ob.args || [2011, 0, 1, 12, 30, 0]);
+    }, 'object');
+
+    equal(Tempus.Interval({ args: [0] }, { args: [1]}), 1, 'Interval uses parsers from Tempus');
+});
+
+covers(Tempus.Interval.prototype, 'Interval', 'diffFullYear', 'diffMonth', 'diffDate', 'diffHours',
+    'diffMinutes', 'diffSeconds', 'diffMilliseconds');
+QUnit.test('diff*()', function () {
+    var newdate = new Tempus(0), newdateclone, interval;
+
+    newdateclone = newdate.clone();
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addYear(2);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     2, '1 extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addMonth(3);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        3, '3 extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addDate(3);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         3, '3 extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addHours(1);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        1, '1 extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addMinutes(3);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      3, '3 extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addSeconds(3);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      3, '3 extra seconds');
+    equal(interval.diffMilliseconds(), 0, 'No extra milliseconds');
+
+    newdateclone = newdate.clone().addMilliseconds(100);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     0, 'No extra years');
+    equal(interval.diffMonth(),        0, 'No extra months');
+    equal(interval.diffDate(),         0, 'No extra days');
+    equal(interval.diffHours(),        0, 'No extra hours');
+    equal(interval.diffMinutes(),      0, 'No extra minutes');
+    equal(interval.diffSeconds(),      0, 'No extra seconds');
+    equal(interval.diffMilliseconds(), 100, '100 extra milliseconds');
+
+    newdateclone = newdate.clone().addYear(1).addMonth(2).addDate(3).addHours(4).
+        addMinutes(5).addSeconds(6).addMilliseconds(7);
+    interval = new Tempus.Interval(newdate, newdateclone);
+
+    ok(1, newdate.toISOString() + ' - ' + newdateclone.toISOString());
+    equal(interval.diffFullYear(),     1, '1 extra years');
+    equal(interval.diffMonth(),        2, '2 extra months');
+    equal(interval.diffDate(),         3, '3 extra days');
+    equal(interval.diffHours(),        4, '4 extra hours');
+    equal(interval.diffMinutes(),      5, '5 extra minutes');
+    equal(interval.diffSeconds(),      6, '6 extra seconds');
+    equal(interval.diffMilliseconds(), 7, '7 extra milliseconds');
+    
+});
+
+covers(Tempus.Interval.prototype, 'Interval', 'toISOString');
+QUnit.test('toISOString()', function () {
+    var newdate = new Tempus();
+
+    var interval = Tempus.Interval(newdate, newdate.clone().addMilliseconds(1));
+    equal(interval.toISOString(), 'PT1MS', 'toISOString() returns ISO interval string (addition)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addMonth(1));
+    equal(interval.toISOString(), 'P1M', 'toISOString() returns ISO interval string (addition)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addYear(1).addMonth(1));
+    equal(interval.toISOString(), 'P1Y1M', 'toISOString() returns ISO interval string (multiple addition)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addMonth(1).addHours(2).addMinutes(30));
+    equal(interval.toISOString(), 'P1MT2H30M', 'toISOString() returns ISO interval string (multiple addition, overlapping units)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addDate(7));
+    equal(interval.toISOString(), 'P7D', 'toISOString() returns ISO interval string (toISOString will never use W)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addYear(1).subMonth(2));
+    equal(interval.toISOString(), 'P10M', 'toISOString() returns ISO interval string (addition and subtraction)');
+
+    interval = Tempus.Interval(newdate.clone().addYear(1), newdate);
+    equal(interval.toISOString(), '-P1Y', 'toISOString() returns ISO interval string (negative dates)');
+
+    interval = Tempus.Interval(newdate.clone().addYear(1).subMonth(1), newdate);
+    equal(interval.toISOString(), '-P11M', 'toISOString() returns ISO interval string (negative dates with addition and subtraction)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addMonth(5));
+    equal(interval.toISOString(), 'P5M', 'crossing DST should not effect toISOString');
+
+});
+
+covers(Tempus.Interval.prototype, 'Interval', 'toString');
+QUnit.test('toString()', function () {
+    var newdate = new Tempus();
+
+    var interval = Tempus.Interval(newdate, newdate.clone().addMilliseconds(1));
+    equal(interval.toString(), 'in 1 millisecond', 'toString() returns human readable string (addition)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addMonth(1));
+    equal(interval.toString(), 'in 1 month', 'toString() returns human readable string (addition)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addYear(1).addMonth(1));
+    equal(interval.toString(), 'in 1 year, 1 month', 'toString() returns human readable string (multiple addition)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addMonth(1).addHours(2).addMinutes(30));
+    equal(interval.toString(), 'in 1 month, 2 hours, 30 minutes', 'toString() returns human readable string (multiple addition, overlapping units)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addDate(7));
+    equal(interval.toString(), 'in 7 days', 'toString() returns human readable string (toString will never use W)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addYear(1).subMonth(2));
+    equal(interval.toString(), 'in 10 months', 'toString() returns human readable string (addition and subtraction)');
+
+    interval = Tempus.Interval(newdate.clone().addYear(1), newdate);
+    equal(interval.toString(), '1 year ago', 'toString() returns human readable string (negative dates)');
+
+    interval = Tempus.Interval(newdate.clone().addYear(1).subMonth(1), newdate);
+    equal(interval.toString(), '11 months ago', 'toString() returns human readable string (negative dates with addition and subtraction)');
+
+    interval = Tempus.Interval(newdate, newdate.clone().addMonth(5));
+    equal(interval.toString(), 'in 5 months', 'crossing DST should not effect toString');
+});
+
+covers(Tempus.Interval.prototype, 'Interval', 'swapDates');
+QUnit.test('swapDates()', function () {
+    var newdate = new Tempus(),
+        newdateclone = newdate.clone().addYear(1),
+        interval = new Tempus.Interval(newdate, newdateclone);
+
+    equal(interval.valueOf(), 31622400000, 'Positive number beore swapDates');
+
+    equal(interval.swapDates().valueOf(), -31622400000, 'Negative number after swapDates');
+});
+
+covers(Tempus.Interval.prototype, 'Interval', 'toJSON');
+QUnit.test('toJSON()', function () {
+    
+    equal(Tempus.Interval.prototype.toJSON === Tempus.Interval.prototype.toISOString, true, 'toJSON is toISOString');
+});
+
